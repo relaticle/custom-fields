b7522711f62ff72e6037bcb4c99b977bce2d3e81
manukminasyan <manuk.minasyan1@gmail.com>
2025-12-09 02:10:34 +0400

    feat: refactor email field to use tags input for multi-value support

--- src/FieldTypeSystem/Definitions/EmailFieldType.php
+++ src/FieldTypeSystem/Definitions/EmailFieldType.php
@@ -19,7 +19,7 @@ class EmailFieldType extends BaseFieldType
 {
     public function configure(): FieldSchema
     {
-        return FieldSchema::string()
+        return FieldSchema::multiChoice()
             ->key('email')
             ->label('Email')
             ->icon('heroicon-o-envelope')
@@ -27,18 +27,15 @@ class EmailFieldType extends BaseFieldType
             ->tableColumn(TextColumn::class)
             ->infolistEntry(TextEntry::class)
             ->priority(15)
-            ->encryptable()
-            ->searchable()
             ->sortable()
-            ->defaultValidationRules([ValidationRule::EMAIL])
+            ->supportsMultiValue()
+            ->withArbitraryValues()
+            ->withoutUserOptions()
             ->availableValidationRules([
                 ValidationRule::REQUIRED,
-                ValidationRule::EMAIL,
                 ValidationRule::MIN,
                 ValidationRule::MAX,
-                ValidationRule::REGEX,
                 ValidationRule::UNIQUE,
-                ValidationRule::EXISTS,
             ]);
     }
 }
