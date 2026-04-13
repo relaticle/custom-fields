e6890d1dbecb2aed3005215e9d277e36d9f8932a
Manuk <manuk.minasyan1@gmail.com>
2026-04-13 23:55:41 +0400

    feat: add FIELD_DESCRIPTION_POSITION feature flag (disabled by default)

--- tests/Feature/DescriptionPositionTest.php
+++ tests/Feature/DescriptionPositionTest.php
@@ -13,3 +13,20 @@ it('returns translatable labels', function (): void {
     expect(DescriptionPosition::BELOW->getLabel())->toBeString()->not->toBeEmpty();
     expect(DescriptionPosition::ABOVE->getLabel())->toBeString()->not->toBeEmpty();
 });
+
+use Relaticle\CustomFields\Enums\CustomFieldsFeature;
+use Relaticle\CustomFields\FeatureSystem\FeatureConfigurator;
+use Relaticle\CustomFields\FeatureSystem\FeatureManager;
+
+it('has FIELD_DESCRIPTION_POSITION feature flag disabled by default', function (): void {
+    expect(FeatureManager::isEnabled(CustomFieldsFeature::FIELD_DESCRIPTION_POSITION))->toBeFalse();
+});
+
+it('can enable FIELD_DESCRIPTION_POSITION feature flag', function (): void {
+    $config = FeatureConfigurator::configure()
+        ->enable(CustomFieldsFeature::FIELD_DESCRIPTION_POSITION);
+
+    config(['custom-fields.features' => $config]);
+
+    expect(FeatureManager::isEnabled(CustomFieldsFeature::FIELD_DESCRIPTION_POSITION))->toBeTrue();
+});
